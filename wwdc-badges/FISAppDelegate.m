420a6773fa04947265ab8806fc3e16c291ea8f20
jmkiley <jordankiley@gmail.com>
2016-05-18 09:36:27 -0400

    Done.

--- wwdc-badges/FISAppDelegate.m
+++ wwdc-badges/FISAppDelegate.m
@@ -13,14 +13,26 @@
      * Write your solution here.
      
      */
-
+    //Advanced
+    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"] ;
     
-    NSArray *conferenceSpeakers = @[@"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"];
-    NSLog(@"%@", conferenceSpeakers);
+    NSMutableArray *firstNames = [ NSMutableArray new ] ;
+    NSMutableArray *lastNames = [ NSMutableArray new ] ;
     
-    for (NSUInteger i = 0; i < [conferenceSpeakers count]; i++) {
-        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i] );
+    for ( NSUInteger i = 0 ; i < [ conferenceSpeakers count ] ; i++ ) {
+        NSArray *splitString = [ conferenceSpeakers[i] componentsSeparatedByString:@" " ] ;
+        [ firstNames addObject:splitString[0] ];
+        [ lastNames addObject:splitString[1] ];
+        NSLog(@"Hello, my name is %@ %@.", firstNames[i], lastNames[i]) ;
     }
+
+//Main
+//    NSArray *conferenceSpeakers = @[ @"Anita Borg", @"Alan Kay", @"Ada Lovelace", @"Aaron Swartz", @"Alan Turing", @"Michael Faraday", @"Grace Hopper", @"Charles Babbage"] ;
+//    for ( NSUInteger i = 0 ; i < [ conferenceSpeakers count ] ; i++ ) {
+//        NSLog(@"Hello, my name is %@.", conferenceSpeakers[i]) ;
+    }
+    
+    
     // do not alter
     return YES;  //
     ///////////////
